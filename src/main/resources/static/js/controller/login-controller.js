48e6da3c1e807a6fb3c40f5c48e3bcdde456c0f7
Jingyu Yao <yjy.emails@gmail.com>
2018-05-09 17:14:55 -0400

    added login controller & css

---
+++ src/main/resources/static/js/controller/login-controller.js
@@ -0,0 +1,36 @@
+(function() {
+  var userServiceClient = new UserServiceClient();
+  var $loginFormUsername;
+  var $loginFormPassword;
+  var $loginFormSubmitBtn;
+  var $errorContainer;
+
+  $(main);
+
+  function main() {
+    $loginFormUsername = $("#login-form-username");
+    $loginFormPassword = $("#login-form-password");
+    $loginFormSubmitBtn = $("#login-form-submit-btn");
+    $errorContainer = $("#error-container");
+
+    $loginFormSubmitBtn.click(login);
+  }
+
+  function login() {
+    $errorContainer.empty();
+
+    var username = $loginFormUsername.val();
+    var password = $loginFormPassword.val();
+    var user = new User(null /* id */, username, password);
+
+    userServiceClient.loginUser(user, loginSuccess, loginFail);
+  }
+
+  function loginSuccess() {
+    window.location.href = "/profile.html";
+  }
+
+  function loginFail(msg) {
+    $errorContainer.text(msg);
+  }
+})();
