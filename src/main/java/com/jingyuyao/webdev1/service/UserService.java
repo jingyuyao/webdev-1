f570fb700783d7b3692945fd13adac188c19f931
Jingyu Yao <yjy.emails@gmail.com>
2018-05-09 16:04:39 -0400

    added register user endpoint

--- src/main/java/com/jingyuyao/webdev1/service/UserService.java
+++ src/main/java/com/jingyuyao/webdev1/service/UserService.java
@@ -2,6 +2,7 @@ package com.jingyuyao.webdev1.service;
 
 import com.jingyuyao.webdev1.model.User;
 import com.jingyuyao.webdev1.repository.UserRepository;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,8 +21,20 @@ public class UserService {
   @Autowired
   private UserRepository userRepository;
 
+  @PostMapping("/api/user/register")
+  public User register(@RequestBody User user, HttpSession session) {
+    if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+      throw new UserExistsException();
+    }
+
+    User saved = userRepository.save(user);
+    // Sets the saved version so it will have an ID.
+    session.setAttribute("user", saved);
+    return saved;
+  }
+
   @PostMapping("/api/user")
-  public User createUser(@RequestBody User user) {
+  public User create(@RequestBody User user) {
     return userRepository.save(user);
   }
 
@@ -78,6 +91,14 @@ public class UserService {
 
     private UserNotFoundException(String username) {
       super("Username" + username + " does not exist");
+    }
+  }
+
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  private class UserExistsException extends RuntimeException {
+
+    private UserExistsException() {
+      super("User already exists");
     }
   }
 }
