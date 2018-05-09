dbf2f79ebda6bf82de8da05b9ec7d1b738228165
Jingyu Yao <yjy.emails@gmail.com>
2018-05-09 14:07:35 -0400

    renamed assignment1 to assignment since this repo will be used for multiple assignments

--- src/main/java/com/jingyuyao/webdev1/Assignment.java
+++ src/main/java/com/jingyuyao/webdev1/Assignment.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignment1 {
+public class Assignment {
 
   public static void main(String[] args) {
-    SpringApplication.run(Assignment1.class, args);
+    SpringApplication.run(Assignment.class, args);
   }
 }
