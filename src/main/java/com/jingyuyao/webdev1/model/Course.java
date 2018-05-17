7e482c1f2f621eeb46cdd04b363db07b7a3d9bf0
Jingyu Yao <yjy.emails@gmail.com>
2018-05-17 14:20:42 -0400

    added course model

---
+++ src/main/java/com/jingyuyao/webdev1/model/Course.java
@@ -0,0 +1,46 @@
+package com.jingyuyao.webdev1.model;
+
+import java.time.Instant;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String title;
+  private Instant created;
+  private Instant modified;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Instant getCreated() {
+    return created;
+  }
+
+  public void setCreated(Instant created) {
+    this.created = created;
+  }
+
+  public Instant getModified() {
+    return modified;
+  }
+
+  public void setModified(Instant modified) {
+    this.modified = modified;
+  }
+}
