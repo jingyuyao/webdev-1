e5f76c210d802e5764e2aa3045f5668ceb78865b
Jingyu Yao <yjy.emails@gmail.com>
2018-05-17 15:01:17 -0400

    added lesson model

--- src/main/java/com/jingyuyao/webdev1/model/Lesson.java
+++ src/main/java/com/jingyuyao/webdev1/model/Lesson.java
@@ -1,18 +1,14 @@
 package com.jingyuyao.webdev1.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
-public class Module {
+public class Lesson {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,9 +16,7 @@ public class Module {
   private String title;
   @ManyToOne
   @JsonIgnore
-  private Course course;
-  @OneToMany(mappedBy = "module", cascade = CascadeType.ALL)
-  private List<Lesson> lessons = new ArrayList<>();
+  private Module module;
 
   public int getId() {
     return id;
@@ -36,11 +30,11 @@ public class Module {
     this.title = title;
   }
 
-  public Course getCourse() {
-    return course;
+  public Module getModule() {
+    return module;
   }
 
-  public void setCourse(Course course) {
-    this.course = course;
+  public void setModule(Module module) {
+    this.module = module;
   }
 }
