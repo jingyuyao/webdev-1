b1b2962e37d58317f7269f293fb932bea8cbdc8b
Jingyu Yao <yjy.emails@gmail.com>
2018-05-18 17:29:02 -0400

    added course service client

--- src/main/js/react/container/CourseManager.js
+++ src/main/js/react/container/CourseManager.js
@@ -1,6 +1,11 @@
 import React from "react";
+import courseService from "../service/CourseService";
 
 class CourseManager extends React.Component {
+  componentWillMount() {
+    courseService.findAll(courses => console.log(courses));
+  }
+
   render() {
     return <h1>Hello world!</h1>;
   }
