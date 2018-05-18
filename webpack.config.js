e270b819aa23a1cffd2c0c2d9e40023106077a01
Jingyu Yao <yjy.emails@gmail.com>
2018-05-18 16:49:45 -0400

    added whatwg-fetch

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/main/js/react/app.js",
+  entry: ["babel-polyfill", "whatwg-fetch", "./src/main/js/react/app.js"],
   devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "./src/main/resources/static/react"),
