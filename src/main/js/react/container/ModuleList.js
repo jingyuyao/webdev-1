eb03da817026968bb67b535b0ef2eba0444e63d2
Jingyu Yao <yjy.emails@gmail.com>
2018-05-19 20:11:45 -0400

    create module form

--- src/main/js/react/container/ModuleList.js
+++ src/main/js/react/container/ModuleList.js
@@ -6,14 +6,50 @@ class ModuleList extends React.Component {
     super(props);
 
     this.state = {
+      newModuleTitle: "",
       modules: []
     };
+
+    this.refreshModules = this.refreshModules.bind(this);
+    this.createNewModule = this.createNewModule.bind(this);
+    this.newModuleTitleChanged = this.newModuleTitleChanged.bind(this);
+    this.removeModule = this.removeModule.bind(this);
   }
 
   componentDidMount() {
+    this.refreshModules();
+  }
+
+  refreshModules() {
     moduleService
       .findAllByCourseId(this.props.courseId)
       .then(modules => this.setState({modules: modules}));
+  }
+
+  createNewModule(event) {
+    event.preventDefault();
+
+    const module = {
+      title: this.state.newModuleTitle,
+      course: {
+        id: this.props.courseId
+      }
+    };
+    this.setState({newModuleTitle: ""});
+
+    moduleService
+      .create(module)
+      .then(this.refreshModules);
+  }
+
+  newModuleTitleChanged(event) {
+    this.setState({newModuleTitle: event.target.value});
+  }
+
+  removeModule(id) {
+    moduleService
+      .remove(id)
+      .then(this.refreshModules);
   }
 
   render() {
@@ -24,6 +60,16 @@ class ModuleList extends React.Component {
     return (
       <div>
         <h3>Modules</h3>
+        <form onSubmit={this.createNewModule}>
+          <label>
+            Title
+            <input
+              type="text"
+              value={this.state.newModuleTitle}
+              onChange={this.newModuleTitleChanged}/>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
         <ul>
           {moduleRows}
         </ul>
