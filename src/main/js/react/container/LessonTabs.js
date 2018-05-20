4b593b8f49245b56761438b0af6ef24e17328173
Jingyu Yao <yjy.emails@gmail.com>
2018-05-19 22:58:31 -0400

    added lesson form

--- src/main/js/react/container/LessonTabs.js
+++ src/main/js/react/container/LessonTabs.js
@@ -8,6 +8,11 @@ class LessonTabs extends React.Component {
     this.state = {
       lessons: []
     };
+
+    this.refreshLessons = this.refreshLessons.bind(this);
+    this.createNewLesson = this.createNewLesson.bind(this);
+    this.newLessonTitleChanged = this.newLessonTitleChanged.bind(this);
+    this.removeLesson = this.removeLesson.bind(this);
   }
 
   componentDidMount() {
@@ -28,13 +33,53 @@ class LessonTabs extends React.Component {
     }
   }
 
+  createNewLesson(event) {
+    event.preventDefault();
+
+    const module = {
+      title: this.state.newLessonTitle,
+      course: {
+        id: this.props.courseId
+      }
+    };
+    this.setState({newLessonTitle: ""});
+
+    moduleService
+      .create(module)
+      .then(this.refreshLessons);
+  }
+
+  newLessonTitleChanged(event) {
+    this.setState({newLessonTitle: event.target.value});
+  }
+
+  removeLesson(id) {
+    moduleService
+      .remove(id)
+      .then(this.refreshLessons);
+  }
+
   render() {
+    const lessonForm = this.props.optModuleId ? (
+      <form onSubmit={this.createNewLesson}>
+        <label>
+          Title
+          <input
+            type="text"
+            value={this.state.newLessonTitle}
+            onChange={this.newLessonTitleChanged}/>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    ) : null;
     const lessonTabs = this.state.lessons.map(lesson =>
       <span key={lesson.id}>{lesson.title}</span>
     );
 
     return (
       <div>
+        <h3>Lessons</h3>
+        {lessonForm}
         {lessonTabs}
       </div>
     );
