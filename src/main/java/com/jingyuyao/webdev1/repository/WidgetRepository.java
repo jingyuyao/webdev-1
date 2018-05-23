f38763bab160d5b0801fc1e8c67c5dee31bea118
Jingyu Yao <yjy.emails@gmail.com>
2018-05-22 21:42:26 -0400

    added widget repository

---
+++ src/main/java/com/jingyuyao/webdev1/repository/WidgetRepository.java
@@ -0,0 +1,8 @@
+package com.jingyuyao.webdev1.repository;
+
+import com.jingyuyao.webdev1.model.Widget;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WidgetRepository extends CrudRepository<Widget, Integer> {
+
+}
