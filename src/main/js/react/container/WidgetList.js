dc8d29fc863f61090737e6fca4ea113e551cf601
Jingyu Yao <yjy.emails@gmail.com>
2018-05-24 16:08:46 -0400

    initial impl of widgets saving frontend

--- src/main/js/react/container/WidgetList.js
+++ src/main/js/react/container/WidgetList.js
@@ -2,11 +2,20 @@ import React from "react";
 import {connect} from "react-redux";
 import {withStyles} from "@material-ui/core/styles";
 import AddBoxIcon from "@material-ui/icons/AddBox";
+import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import widgetService from "../service/WidgetService";
 import * as WidgetsActions from "../action/WidgetsActions";
 
 const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit
+  },
+  header: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "flex-end"
+  },
   footer: {
     alignItems: "center",
     display: "flex",
@@ -22,6 +31,7 @@ function mapStateToProps(state) {
   widgetsToDisplay.sort((l, r) => l.position - r.position);
 
   return {
+    widgets: state.widgets,
     widgetsToDisplay: widgetsToDisplay
   };
 }
@@ -51,6 +61,7 @@ class WidgetList extends React.Component {
 
     this.refreshWidgets = this.refreshWidgets.bind(this);
     this.addWidget = this.addWidget.bind(this);
+    this.saveWidgets = this.saveWidgets.bind(this);
   }
 
   componentDidMount() {
@@ -79,11 +90,37 @@ class WidgetList extends React.Component {
     this.props.widgetsAdd(defaultWidget);
   }
 
+  saveWidgets() {
+    this.props.widgetsSaving();
+
+    const toAddPromises =
+      this.props.widgets.toAdd.map(
+        widget => widgetService.create(this.props.lessonId, widget));
+    const toDeletePromises =
+      this.props.widgets.toDelete.map(widget => widgetService.remove(widget));
+    const toUpdatePromises =
+      this.props.widgets.active.map(widget => widgetService.update(widget.id, widget));
+    const allPromises = [
+      ...toAddPromises,
+      ...toDeletePromises,
+      ...toUpdatePromises
+    ];
+    Promise.all(allPromises).then(this.refreshWidgets);
+  }
+
   render() {
     const classes = this.props.classes;
 
     return (
-      <React.Fragment>
+      <div className={classes.root}>
+        <div className={classes.header}>
+          <Button
+            variant="raised" color="primary"
+            disabled={this.props.widgets.saving}
+            onClick={this.saveWidgets}>
+            {this.props.widgets.saving ? "Saving" : "Save"}
+          </Button>
+        </div>
         <ul>
           {this.props.widgetsToDisplay.map(widget => (
             <li key={widget.position}>{widget.type}</li>
@@ -94,7 +131,7 @@ class WidgetList extends React.Component {
             <AddBoxIcon/>
           </IconButton>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
